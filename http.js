321d8cb910d494f1f227c89d1982ffad81e49fe6
Mayank Thakur <mt2655273@gmail.com>
2024-10-27 18:02:28 +0530

    Removing express module

--- http.js
+++ http.js
@@ -1,7 +1,6 @@
 const http = require("http")
 const fs = require("fs")
 const url = require("url")
-const express = require("express")
 const myserver = http.createServer((req, res) => {
     if (req.url === "/favicon.ico") return res.end()
     const log = `${Date.now()}: ${req.method} ${req.url} New Request Received\n`
